5b03aa0251dba9d0415d19aa9c60b12019f75bd5
isimbiJessica <jessicaritha@gmail.com>
2019-08-01 15:10:32 +0200

    adding some images

--- js/script.js
+++ js/script.js
@@ -1,35 +1,10 @@
 $(document).ready(function(){
-    $("#h1").hover(function() {
-        $("#h1").css("opacity","0.2");
-        $("#une").show();
-    },
-    function(){
-        $("#h1").css("opacity","1");
-        $("#une").hide();
+    $(".all").click(function(){
+        $("#p1").toggle();
+        $("#img1").toggle();
     });
-    $("#h2").hover(function() {
-        $("#h2").css("opacity","0.2");
-        $("#deux").show();
-    },
-    function(){
-        $("#h2").css("opacity","1");
-        $("#deux").hide();
+    $(".all1").click(function(){
+        $("#p2").toggle();
+        $("#img2").toggle();
     });
-    $("#h3").hover(function() {
-        $("#h3").css("opacity","0.2");
-        $("#trois").show();
-    },
-    function(){
-        $("#h3").css("opacity","1");
-        $("#trois").hide();
-    });
-    $("#h4").hover(function() {
-        $("#h4").css("opacity","0.2");
-        $("#quatre").show();
-    },
-    function(){
-        $("#h4").css("opacity","1");
-        $("#quatre").hide();
-    });
-
-});
+})
